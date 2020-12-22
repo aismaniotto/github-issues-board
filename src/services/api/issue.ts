08ea1823b1995ad6f4e6d5edaca50de81dbd0d9a
aismaniotto <aismaniotto@gmail.com>
2020-12-21 22:03:30 -0300

    fix: issue and label uri to api

--- src/services/api/issue.ts
+++ src/services/api/issue.ts
@@ -2,7 +2,7 @@ import api from '.';
 import { Issue } from '../../store/modules/board/types';
 
 export const getIssues = (owner: string, repo: string) =>
-  api.get(`/repo/${owner}/${repo}/issues`);
+  api.get(`/repos/${owner}/${repo}/issues`);
 
 export const newIssue = (owner: string, repo: string, issue: Issue) => {
   const body = {
@@ -10,7 +10,7 @@ export const newIssue = (owner: string, repo: string, issue: Issue) => {
     body: issue.body,
     labels: issue.labels?.map((label) => label.name),
   };
-  return api.post(`/repo/${owner}/${repo}/issues`, body);
+  return api.post(`/repos/${owner}/${repo}/issues`, body);
 };
 
 export const updateIssue = (owner: string, repo: string, issue: Issue) => {
@@ -19,5 +19,5 @@ export const updateIssue = (owner: string, repo: string, issue: Issue) => {
     body: issue.body,
     labels: issue.labels?.map((label) => label.name),
   };
-  return api.post(`/repo/${owner}/${repo}/issues/${issue.number}`, body);
+  return api.post(`/repos/${owner}/${repo}/issues/${issue.number}`, body);
 };
--- src/services/api/label.ts
+++ src/services/api/label.ts
@@ -1,4 +1,4 @@
 import api from '.';
 
 export const getLabels = (owner: string, repo: string) =>
-  api.get(`/repo/${owner}/${repo}/labels`);
+  api.get(`/repos/${owner}/${repo}/labels`);
