ec6f47a2a35814c721600932d50ec8f3232b4a74
aismaniotto <aismaniotto@gmail.com>
2021-01-04 17:16:19 -0300

    feat(issue): open github to see issue details

--- src/components/draggable-issue-card.tsx
+++ src/components/draggable-issue-card.tsx
@@ -1,6 +1,8 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { getCurrentRepoOwner } from '../services/local-storage/repoOwner';
+import { getCurrentRepository } from '../services/local-storage/repository';
 import { Issue } from '../store/modules/issue/types';
 import Style from '../styles/components/draggable-issue-card';
 import Tag from './tag';
@@ -13,10 +15,16 @@ interface Props {
 const DraggableIssueCard: React.FC<Props> = (props: Props) => {
   const { issue, index } = props;
   const classes = Style();
+
+  const currentRepoOwner = getCurrentRepoOwner();
+  const currentRepository = getCurrentRepository();
+
   return (
     <Draggable key={issue.number} draggableId={`${issue.number}`} index={index}>
       {(provided) => (
         <div
+          onClick={() => window.open(`https://github.com/${currentRepoOwner}/${currentRepository}/issues/${issue.number}`, '_blank')}
+          role="presentation"
           className={classes.root}
           ref={provided.innerRef}
           // eslint-disable-next-line react/jsx-props-no-spreading
